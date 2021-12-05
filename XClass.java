9e735b4d3b08d5914f1ab2e48ca2a74471f2ceea
Abhishek Sai <84014492+abhisai2895@users.noreply.github.com>
2021-12-05 14:12:43 +0530

    Add files via upload

---
+++ XClass.java
@@ -0,0 +1,10 @@
+public class XClass
+{
+    public void prnCube() 
+    {
+        int sum=0, i;
+        for(int i = i;i<10;i+=2)
+        sum += (i*i*i);
+        System.out.println("Sum of the cubes of odd number between 1..10  is: " + sum);
+    }
+}
